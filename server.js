6ca52baa4dc150b923f1730522cec22a56a6fe12
v3xxed <oskarasbirbalas@gmail.com>
2023-04-19 18:14:18 +0100

    express setup

---
+++ server.js
@@ -0,0 +1,39 @@
+//Setup Express ------------------------------------------------------------------------- 
+const express = require('express');
+const app = express();
+
+//sets up express session so that the user can remain signed in
+const session = require('express-session'); 
+
+//sets up the body parser
+const bodyParser = require('body-parser');
+
+app.use(express.urlencoded({extended: true}))
+
+//sets the view engine to ejs so we can make use of templates
+app.set('view engine', 'ejs');
+
+//allows the use of static elements
+app.use(express.static('public'));
+
+//variable to hold the database
+var db;
+
+app.use(session({
+    secret : 'example',
+    resave: true,
+    saveUninitialized: true
+  }));
+
+  
+//tells express we want to read posted forms
+app.use(bodyParser.urlencoded({
+    extended: true
+  }))
+
+app.use(bodyParser.json());
+
+app.use(function(req, res, next){
+  res.locals.username = req.session.currentuser
+  next();
+  })
